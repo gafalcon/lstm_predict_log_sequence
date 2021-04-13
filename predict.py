33cf9069f34969d2435a0d59737c5afd989a0d01
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-04-13 11:31:47 -0400

    predict next action

--- predict.py
+++ predict.py
@@ -31,21 +31,44 @@ def barplot(x, y, seq):
 
 seq = []
 seq_x = []
-action = 'start'
-seq.append(action)
-new_input = create_input(action)
-seq_x.append(new_input)
-x = np.array(seq_x, dtype=float)
-batch_x = torch.Tensor(np.expand_dims(x, 0))
+init_seq = ['start']
+action = None
 
-#Predict
-out, state = model(batch_x)
-y_pred = out[0]
-p = nn.functional.softmax(y_pred, dim=1).detach().numpy()
-sorted_idx = p.argsort(axis=1)
-top_5_idx = sorted_idx[0][::-1][:5]
-top_5_p = p[0][top_5_idx]
-top_5_seq_types = seq_types[top_5_idx]
-predicted_seq_type =seq_types[p.argmax(axis=1)]
-barplot(top_5_seq_types, top_5_p, seq)
-print("predicted: ", predicted_seq_type)
+def initialize_seq(init_seq):
+    global seq, seq_x
+    seq = []
+    seq_x = []
+    for action in init_seq:
+        seq.append(action)
+        new_input = create_input(action)
+        seq_x.append(new_input)
+
+
+def predict_next_action(newAction=None):
+    global action
+    if newAction is not None:
+        action = newAction
+    if action is not None:
+        seq.append(action)
+        new_input = create_input(action)
+        seq_x.append(new_input)
+    x = np.array(seq_x, dtype=float)
+    batch_x = torch.Tensor(np.expand_dims(x, 0))
+
+    #Predict
+    out, state = model(batch_x)
+    y_pred = out[0][-1]
+    p = nn.functional.softmax(y_pred, dim=0).detach().numpy()
+    sorted_idx = p.argsort()
+    top_5_idx = sorted_idx[::-1][:5]
+    top_5_p = p[top_5_idx]
+    top_5_seq_types = seq_types[top_5_idx]
+    predicted_seq_type =seq_types[p.argmax()]
+    barplot(top_5_seq_types, top_5_p, seq)
+    print("sequence:", seq)
+    print("predicted: ", predicted_seq_type)
+    action = predicted_seq_type
+
+
+initialize_seq(init_seq)
+predict_next_action()
